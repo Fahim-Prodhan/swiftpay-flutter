bb81ccf116147cb85c99c9622cc89640cb370b8b
rshahriar0075 <rshahriar075@gmail.com>
2025-04-14 01:11:22 +0600

    SendMoney initialize

---
+++ lib/pages/send_money.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class sendMoneyPage extends StatefulWidget {
+  const sendMoneyPage({super.key});
+
+  @override
+  State<sendMoneyPage> createState() => _sendMoneyPageState();
+}
+
+class _sendMoneyPageState extends State<sendMoneyPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
